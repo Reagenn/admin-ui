0709a124801413e5f5596e635f51a8753d55366f
Reagenn <regancaesarsuseno@gmail.com>
2024-10-20 11:26:57 +0700

    y

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,5 +1,5 @@
 import FormSignUp from "../component/Fragments/FormSignUp";
-import Authlayout from "../component/Layouts/Authlayout";
+import Authlayout from "../component/Layouts/Authlayout.jsx";
 
 const SignUpPage = () => {
   return (
