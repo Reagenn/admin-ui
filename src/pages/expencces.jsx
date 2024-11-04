31b5f51b587e4e70d1567dd5395278dc62da49c2
Reagenn <regancaesarsuseno@gmail.com>
2024-11-04 17:02:59 +0700

    update

--- src/pages/expencces.jsx
+++ src/pages/expencces.jsx
@@ -6,7 +6,7 @@ const ExpenccesPage = () => {
     <MainLayout type="expencces">
       <h1 className="text-gray-500 text-2xl mb-4">Expencces Comparison</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 gap-6">
-        <CardC cardTitle="Credit Card" cardType="Master Card" logoSrc="/images/mc.png" accountNumber="1234 5678 9012 8***" totalAmount="$25,000" />
+        <CardC desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos nisi praesentium magni in placeat dolores quo aperiam, tenetur corporis est vitae eveniet distinctio rem illum accusantium, repellendus consectetur sed!" />
       </div>
       <br />
       <h1 className="text-gray-500 text-2xl mb-4">Expencces Breakdown</h1>
