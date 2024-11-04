59562f2ca07852d88570b9c078843f470f10aabd
Reagenn <regancaesarsuseno@gmail.com>
2024-11-04 17:01:06 +0700

    update terbaru

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,28 +4,22 @@ const DashboardPage = () => {
   return (
     <MainLayout type="dashboard">
       {/* top content start*/}
-      <div className="mb-8 sm:flex sm:gap-6">
-        <div className="sm:w-1/3">
-          <Card />
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <Card title="Total Balance" />
+        <Card
+          title="Goals"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos nisi praesentium magni in placeat dolores quo aperiam, tenetur corporis est vitae eveniet distinctio rem illum accusantium, repellendus consectetur sed!"
+        />
+        <Card title="Upcoming Bill" />
+        <div className="md:col-span-1">
+          <Card
+            title="Recent Transaction"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos nisi praesentium magni in placeat dolores quo aperiam, tenetur corporis est vitae eveniet distinctio rem illum accusantium, repellendus consectetur sed!"
+          />
         </div>
-        <div className="sm:w-1/3">
-          <Card />
-        </div>
-        <div className="sm:w-1/3">
-          <Card />
-        </div>
-      </div>
-      {/* top content end*/}
-      {/* bottom content start*/}
-      <div className="mb-8 sm:flex sm:gap-6">
-        <div className="sm:w-1/3">
-          <Card />
-        </div>
-        <div className="sm:w-1/3">
-          <Card />
-        </div>
-        <div className="sm:w-1/3">
-          <Card />
+        <div className="md:col-span-2 flex flex-col flex-1">
+          <Card title="Statistics" />
+          <Card title="Expenses Breakdown" />
         </div>
       </div>
 
--- src/pages/expencces.jsx
+++ src/pages/expencces.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import CardC from "../component/Elements/CardC";
+import MainLayout from "../component/Layouts/MainLayout";
 
-const expencces = () => {
-  return <div>expencces</div>;
+const ExpenccesPage = () => {
+  return (
+    <MainLayout type="expencces">
+      <h1 className="text-gray-500 text-2xl mb-4">Expencces Comparison</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-1 gap-6">
+        <CardC cardTitle="Credit Card" cardType="Master Card" logoSrc="/images/mc.png" accountNumber="1234 5678 9012 8***" totalAmount="$25,000" />
+      </div>
+      <br />
+      <h1 className="text-gray-500 text-2xl mb-4">Expencces Breakdown</h1>
+
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <CardC title="Total Balance" />
+        <CardC
+          title="Goals"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos nisi praesentium magni in placeat dolores quo aperiam, tenetur corporis est vitae eveniet distinctio rem illum accusantium, repellendus consectetur sed!"
+        />
+        <CardC title="Upcoming Bill" />
+      </div>
+      <br />
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <CardC title="Total Balance" />
+        <CardC
+          title="Goals"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos nisi praesentium magni in placeat dolores quo aperiam, tenetur corporis est vitae eveniet distinctio rem illum accusantium, repellendus consectetur sed!"
+        />
+        <CardC title="Upcoming Bill" />
+      </div>
+    </MainLayout>
+  );
 };
 
-export default expencces;
+export default ExpenccesPage;
